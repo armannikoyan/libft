dd92311919642ecee1b553391aa6ca7413aac35f
Arman Nikoyan <armannikoyan04@icloud.com>
2025-01-09 08:42:26 -0500

    fixed: ft_strcmp not working properly and getting out of range

--- src/ft_strcmp.c
+++ src/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: anikoyan <anikoyan@student.42yerevan.am>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 16:44:56 by anikoyan          #+#    #+#             */
-/*   Updated: 2024/12/17 11:45:48 by anikoyan         ###   ########.fr       */
+/*   Updated: 2025/01/09 08:40:08 by anikoyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strcmp(const char *s1, const char *s2)
 	int	i;
 
 	i = 0;
-	while (s1[i] == s2[i] && s1[i] && s2[i])
+	while (s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
