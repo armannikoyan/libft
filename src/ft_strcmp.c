dbae3397efbda851c267496a6ea33fc5c456d056
Arman Nikoyan <armannikoyan04@icloud.com>
2025-01-09 15:25:47 -0500

    fixed: crashed when NULL passed

--- src/ft_strcmp.c
+++ src/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: anikoyan <anikoyan@student.42yerevan.am>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 16:44:56 by anikoyan          #+#    #+#             */
-/*   Updated: 2025/01/09 08:40:08 by anikoyan         ###   ########.fr       */
+/*   Updated: 2025/01/09 08:47:06 by anikoyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int	ft_strcmp(const char *s1, const char *s2)
 {
 	int	i;
 
+	if (!s1 || !s2)
+		return (-1);
 	i = 0;
 	while (s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
