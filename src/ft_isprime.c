f32b45bfe75bd769c170e0218ae4713ad43c45f5
Arman Nikoyan <armannikoyan04@icloud.com>
2025-03-19 12:02:50 +0400

    changed: ft_isprime calculates % 2 using bitwise operators

--- src/ft_isprime.c
+++ src/ft_isprime.c
@@ -6,7 +6,7 @@
 /*   By: anikoyan <anikoyan@student.42yerevan.am>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 17:32:33 by anikoyan          #+#    #+#             */
-/*   Updated: 2025/03/17 20:41:42 by anikoyan         ###   ########.fr       */
+/*   Updated: 2025/03/19 11:58:11 by anikoyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ bool	ft_isprime(int number)
 {
 	size_t	i;
 
-	if (number <= 1 || number % 2 == 0 || number % 3 == 0)
+	if (number <= 1 || !(number & 1) || number % 3 == 0)
 		return (false);
 	else if (number <= 3)
 		return (true);
